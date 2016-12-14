0ce787b376340bc3be3f4f5a0c9054ec540030cf
Fonzo <aggadur@gmail.com>
2016-12-14 13:22:12 +0100

    Webhook Intern

--- IFTTT IO/module.php
+++ IFTTT IO/module.php
@@ -138,22 +138,35 @@ class IFTTTIO extends IPSModule
 	
 	protected function SetIFTTTInterface()
 		{
-			//prüfen ob Script existent
-				$SkriptID = @$this->GetIDForIdent("IFTTTIPSInterface");
-				
-				if ($SkriptID === false)
+			$ipsversion = $this->GetIPSVersion();
+			if($ipsversion == 0 || $ipsversion == 1)
+				{
+					//prüfen ob Script existent
+					$SkriptID = @IPS_GetObjectIDByIdent("IFTTTIPSInterface", $this->InstanceID);
+					if ($SkriptID === false)
+						{
+							$ID = $this->RegisterScript("IFTTTIPSInterface", "IFTTT IPS Interface", $this->CreateWebHookScript(), 4);
+							IPS_SetHidden($ID, true);
+							$this->RegisterHookOLD('/hook/IFTTT', $ID);
+						}
+					else
+						{
+							//echo "Die Skript-ID lautet: ". $SkriptID;
+						}
+				}
+			else
+				{
+					$SkriptID = @IPS_GetObjectIDByIdent("IFTTTIPSInterface", $this->InstanceID);
+					if ($SkriptID > 0)
 					{
-						$ID = $this->RegisterScript("IFTTTIPSInterface", "IFTTT IPS Interface", $this->CreateWebHookScript(), 4);
-						IPS_SetHidden($ID, true);
-						$this->RegisterHook('/hook/IFTTT', $ID);
+						$this->UnregisterHook("/hook/IFTTT");
+						$this->UnregisterScript("IFTTTIPSInterface");
 					}
-				else
-					{
-						//echo "Die Skript-ID lautet: ". $SkriptID;
-					}
+					$this->RegisterHook("/hook/IFTTT");
+				}
 		}
 	
-	private function RegisterHook($WebHook, $TargetID)
+	private function RegisterHookOLD($WebHook, $TargetID)
 		{
 			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
 			if (sizeof($ids) > 0)
@@ -179,16 +192,88 @@ class IFTTTIO extends IPSModule
 			}
 		}
 		
+		private function RegisterHook($WebHook)
+		{
+  			$ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
+  			if(sizeof($ids) > 0)
+				{
+  				$hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
+  				$found = false;
+  				foreach($hooks as $index => $hook)
+					{
+					if($hook['Hook'] == $WebHook)
+						{
+						if($hook['TargetID'] == $this->InstanceID)
+  							return;
+						$hooks[$index]['TargetID'] = $this->InstanceID;
+  						$found = true;
+						}
+					}
+  				if(!$found)
+					{
+ 					$hooks[] = Array("Hook" => $WebHook, "TargetID" => $this->InstanceID);
+					}
+  				IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
+  				IPS_ApplyChanges($ids[0]);
+				}
+  		}
+		
+	/**
+     * Löscht einen WebHook, wenn vorhanden.
+     *
+     * @access private
+     * @param string $WebHook URI des WebHook.
+     */
+    protected function UnregisterHook($WebHook)
+    {
+        $ids = IPS_GetInstanceListByModuleID("{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}");
+        if (sizeof($ids) > 0)
+        {
+            $hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
+            $found = false;
+            foreach ($hooks as $index => $hook)
+            {
+                if ($hook['Hook'] == $WebHook)
+                {
+                    $found = $index;
+                    break;
+                }
+            }
+            if ($found !== false)
+            {
+                array_splice($hooks, $index, 1);
+                IPS_SetProperty($ids[0], "Hooks", json_encode($hooks));
+                IPS_ApplyChanges($ids[0]);
+            }
+        }
+    }  
+	
+	/**
+     * Löscht eine Script, sofern vorhanden.
+     *
+     * @access private
+     * @param int $Ident Ident der Variable.
+     */
+    protected function UnregisterScript($Ident)
+    {
+        $sid = @IPS_GetObjectIDByIdent($Ident, $this->InstanceID);
+        if ($sid === false)
+            return;
+        if (!IPS_ScriptExists($sid))
+            return; //bail out
+        IPS_DeleteScript($sid, true);
+    } 
+		
 	private function CreateWebHookScript()
 		{
         $Script = '<?
 //Do not delete or modify.
-IFTTTIO_ProcessHookData('.$this->InstanceID.');		
+IFTTTIO_ProcessHookDataOLD('.$this->InstanceID.');		
 ?>';	
 		return $Script;
 		}	
 		
-		public function ProcessHookData()
+		public function ProcessHookDataOLD()
 		{
 			$webhookusername = $this->ReadPropertyString('username');
 			$webhookpassword = $this->ReadPropertyString('password');
@@ -208,7 +293,7 @@ IFTTTIO_ProcessHookData('.$this->InstanceID.');
 			 
 			if(($_SERVER['PHP_AUTH_USER'] != $webhookusername) || ($_SERVER['PHP_AUTH_PW'] != $webhookpassword))
 				{
-				header('WWW-Authenticate: Basic Realm="Flow WebHook"');
+				header('WWW-Authenticate: Basic Realm="IFTTT WebHook"');
 				header('HTTP/1.0 401 Unauthorized');
 				echo "Authorization required";
 				return;
@@ -304,7 +389,53 @@ IFTTTIO_ProcessHookData('.$this->InstanceID.');
 				}
 				*/
 		}
+	
+	/**
+ 	* This function will be called by the hook control. Visibility should be protected!
+  	*/
 		
+	protected function ProcessHookData()
+	{
+		$webhookusername = $this->ReadPropertyString('username');
+			$webhookpassword = $this->ReadPropertyString('password');
+			//workaround for bug
+			if(!isset($_IPS))
+				global $_IPS;
+			if($_IPS['SENDER'] == "Execute")
+				{
+				echo "This script cannot be used this way.";
+				return;
+				} 
+			
+			if(!isset($_SERVER['PHP_AUTH_USER']))
+				$_SERVER['PHP_AUTH_USER'] = "";
+			if(!isset($_SERVER['PHP_AUTH_PW']))
+				$_SERVER['PHP_AUTH_PW'] = "";
+			 
+			if(($_SERVER['PHP_AUTH_USER'] != $webhookusername) || ($_SERVER['PHP_AUTH_PW'] != $webhookpassword))
+				{
+				header('WWW-Authenticate: Basic Realm="IFTTT WebHook"');
+				header('HTTP/1.0 401 Unauthorized');
+				echo "Authorization required";
+				return;
+				}
+			echo "Webhook IFTTT IP-Symcon 4";
+			
+			//workaround for bug
+			if(!isset($_IPS))
+				global $_IPS;
+			if($_IPS['SENDER'] == "Execute")
+				{
+				echo "This script cannot be used this way.";
+				return;
+				} 
+
+			 # Capture JSON content
+			$iftttjson = file_get_contents('php://input');
+			$data = json_decode($iftttjson);
+			$this->SendJSON($data);
+			IPS_LogMessage("IFTTT I/O:", utf8_decode($iftttjson)." empfangen.");
+	}
 	
 	################## SEMAPHOREN Helper  - private
 
@@ -329,6 +460,26 @@ IFTTTIO_ProcessHookData('.$this->InstanceID.');
           IPS_SemaphoreLeave("IFTTT_" . (string) $this->InstanceID . (string) $ident);
     }
 	
+	protected function GetIPSVersion ()
+		{
+			$ipsversion = IPS_GetKernelVersion ( );
+			$ipsversion = explode( ".", $ipsversion);
+			$ipsmajor = intval($ipsversion[0]);
+			$ipsminor = intval($ipsversion[1]);
+			if($ipsminor < 10) // 4.0
+			{
+				$ipsversion = 0;
+			}
+			elseif ($ipsminor >= 10 && $ipsminor < 20) // 4.1
+			{
+				$ipsversion = 1;
+			}
+			else   // 4.2
+			{
+				$ipsversion = 2;
+			}
+			return $ipsversion;
+		}
 
 }
 
