36e1905a75760d3e0cda2b781bc18e11d1b16284
Fonzo <wolbolar@gmail.com>
2019-07-29 09:41:29 +0000

    Apply fixes from StyleCI

--- IFTTT Splitter/module.php
+++ IFTTT Splitter/module.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 class IFTTTSplitter extends IPSModule
 {
-
     public function Create()
     {
         //Never delete this line!
@@ -10,7 +11,7 @@ class IFTTTSplitter extends IPSModule
 
         //These lines are parsed on Symcon Startup or Instance creation
         //You cannot use variables here. Just static values.
-        $this->RequireParent("{2E91373A-E70B-46D8-99A7-71A499F6783A}"); //IFTTT I/O
+        $this->RequireParent('{2E91373A-E70B-46D8-99A7-71A499F6783A}'); //IFTTT I/O
     }
 
     public function ApplyChanges()
@@ -28,9 +29,7 @@ class IFTTTSplitter extends IPSModule
 
     /**
      * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-     * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-     *
-     *
+     * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:.
      */
 
     // Data an Child weitergeben
@@ -42,20 +41,17 @@ class IFTTTSplitter extends IPSModule
         // Empfangene Daten vom IFTTT I/O
         $data   = json_decode($JSONString);
         $dataio = json_encode($data->Buffer);
-        $this->SendDebug("ReceiveData IFTTT Splitter:", $dataio, 0);
+        $this->SendDebug('ReceiveData IFTTT Splitter:', $dataio, 0);
 
         // Hier werden die Daten verarbeitet
 
-
         // Weiterleitung zu allen Gerät-/Device-Instanzen
         $this->SendDataToChildren(
-            json_encode(["DataID" => "{F294A60D-30AF-4452-9A17-44A89EBE6ADE}", "Buffer" => $data->Buffer])
+            json_encode(['DataID' => '{F294A60D-30AF-4452-9A17-44A89EBE6ADE}', 'Buffer' => $data->Buffer])
         ); //IFTTT Splitter Interface GUI
     }
 
-
-    ################## DATAPOINT RECEIVE FROM CHILD
-
+    //################# DATAPOINT RECEIVE FROM CHILD
 
     // Type String, Declaration can be used when PHP 7 is available
     //public function ForwardData(string $JSONString)
@@ -66,31 +62,30 @@ class IFTTTSplitter extends IPSModule
         $data     = json_decode($JSONString);
         $datasend = $data->Buffer;
         $datasend = json_encode($datasend);
-        $this->SendDebug("IFTTT Splitter Forward Data:", $datasend, 0);
+        $this->SendDebug('IFTTT Splitter Forward Data:', $datasend, 0);
 
         // Hier würde man den Buffer im Normalfall verarbeiten
         // z.B. CRC prüfen, in Einzelteile zerlegen
         try {
             // Weiterleiten zur I/O Instanz
             $resultat =
-                $this->SendDataToParent(json_encode(["DataID" => "{0259663C-D915-4A86-902B-70D865662E78}", "Buffer" => $data->Buffer])); //TX GUI
+                $this->SendDataToParent(json_encode(['DataID' => '{0259663C-D915-4A86-902B-70D865662E78}', 'Buffer' => $data->Buffer])); //TX GUI
         } catch (Exception $ex) {
             echo $ex->getMessage();
             echo ' in ' . $ex->getFile() . ' line: ' . $ex->getLine() . '.';
         }
-
 
         // Weiterverarbeiten und durchreichen
         return $resultat;
 
     }
 
-    ################## SEMAPHOREN Helper  - private
+    //################# SEMAPHOREN Helper  - private
 
     private function lock($ident)
     {
         for ($i = 0; $i < 3000; $i++) {
-            if (IPS_SemaphoreEnter("IFTTT_" . (string) $this->InstanceID . (string) $ident, 1)) {
+            if (IPS_SemaphoreEnter('IFTTT_' . (string) $this->InstanceID . (string) $ident, 1)) {
                 return true;
             } else {
                 IPS_Sleep(mt_rand(1, 5));
@@ -101,6 +96,6 @@ class IFTTTSplitter extends IPSModule
 
     private function unlock($ident)
     {
-        IPS_SemaphoreLeave("IFTTT_" . (string) $this->InstanceID . (string) $ident);
+        IPS_SemaphoreLeave('IFTTT_' . (string) $this->InstanceID . (string) $ident);
     }
 }
